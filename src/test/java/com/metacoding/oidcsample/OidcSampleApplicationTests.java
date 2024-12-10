635f5e664bc5bfc4a4911736029b14f470f4071e
lnk <nakyeom0620@gmail.com>
2024-12-10 14:01:49 +0900

    OAuth구현하기 중 oidc기본키 검증

---
+++ src/test/java/com/metacoding/oidcsample/OidcSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metacoding.oidcsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
